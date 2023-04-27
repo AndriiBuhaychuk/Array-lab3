5893540df161ecfed09264d52f0e30b60fa3e3db
AndriiBuhaychuk <117983189+AndriiBuhaychuk@users.noreply.github.com>
2023-04-27 20:37:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        
+        int[] arr = { 3, 6, 1, 5, 2, 7, 11, 8 };
+
+        
+        int maxIndex = 0;
+        int minIndex = 0;
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > arr[maxIndex])
+            {
+                maxIndex = i;
+            }
+            else if (arr[i] < arr[minIndex])
+            {
+                minIndex = i;
+            }
+        }
+
+        
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[minIndex];
+        arr[minIndex] = temp;
+
+        
+        Console.WriteLine(string.Join(", ", arr));
+    }
+}
